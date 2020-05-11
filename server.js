ea989e4089282f07810fc2b098dffad14d572d38
fbslo <admin@fbslo.net>
2020-05-11 17:56:37 +0200

    fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/', require('./routes/index.js'));
 app.use('/api', require('./routes/api.js'));
 
 
-hive.config.set('alternative_api_endpoints', ['https://api.hive.blog', 'https://anyx.io']);
+hive.config.set('alternative_api_endpoints', ['https://anyx.io', 'https://api.hive.blog', 'https://api.pharesim.me', 'https://rpc.ausbit.dev', 'https://hived.privex.io', 'https://api.openhive.network', 'https://techcoderx.com', 'https://rpc.esteem.app']);
 
 var con = require('./database/database.js')
 var stream = require('./scripts/streamBlockchain.js')
